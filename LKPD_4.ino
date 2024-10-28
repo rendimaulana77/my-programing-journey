102893b37e536da639dcf04ba7c876edf537bb22
rendimaulana77 <rendimaulanaa525@gmail.com>
2024-10-28 14:48:38 +0700

    buzzer
    hasil praktikum membunyikan buzzer selama 1 detik dan mematikan buzzer selama 1 detik

---
+++ LKPD_4.ino
@@ -0,0 +1,11 @@
+//@pakgurud9
+#define BUZZER_PIN 27
+void setup() {
+  pinMode(BUZZER_PIN, OUTPUT);
+}
+void loop() {
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(1000);
+  digitalWrite(BUZZER_PIN, LOW);
+  delay(1000);
+}
